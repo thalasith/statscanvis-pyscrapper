2888e051a961863a6b6543db6d1d732dc2446afb
Boris Tsao <boris.tsao@gmail.com>
2023-01-10 14:20:50 -0800

    testing out the new refactor

--- library/data_pipeline.py
+++ library/data_pipeline.py
@@ -13,26 +13,25 @@ except:
 bold_green = "\x1b[32;1m"
 reset = "\x1b[0m"
 
-def data_pipeline_job(pid, table_name, pick_members_1, pick_members_2,filter_names) :
+def data_pipeline_job(pid, table_name, pick_members_dict,filter_names):
     today = date.today()
     four_months_ago = date.today() - relativedelta(months=+4)
     start_month=str(four_months_ago.month).zfill(2)
     start_year=str(four_months_ago.year)
     end_month=str(today.month).zfill(2)
     end_year=str(today.year)
-
-    referencePeriods = start_year + start_month + "01" + "%2C" + end_year + end_month + "28" 
-    pick_members_1_dict = dict(zip(pick_members_1["names"], pick_members_1["values"]))
-    pick_members_2_dict = dict(zip(pick_members_2["names"], pick_members_2["values"]))
-
-    result = [(x, y) for x in pick_members_1["names"] for y in pick_members_2["names"]]
-    
     conn_string = 'mysql+pymysql://' + os.environ["USERNAME"] + ':' + os.environ["PASSWORD"] + '@' + os.environ["HOST"] + '/' + os.environ["DATABASE"] 
 
-    for x, y in result:
-        geography=pick_members_1_dict[x]
-        type_of_employee=pick_members_2_dict[y]
-        url = 'https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=' + pid + '&pickMembers%5B0%5D='+ x + '&pickMembers%5B1%5D='+ y + '&cubeTimeFrame.startMonth='+ start_month + '&cubeTimeFrame.startYear=' + start_year + '&cubeTimeFrame.endMonth=' + end_month +'&cubeTimeFrame.endYear=' + end_year + '&referencePeriods=' + referencePeriods
+    referencePeriods = start_year + start_month + "01" + "%2C" + end_year + end_month + "28" 
+    keys = list(pick_members_dict.keys())
+    values = list((pick_members_dict[key]["names"] for key in keys))
+    
+    pick_members_1_dict = dict(zip(pick_members_dict["&pickMembers%5B0%5D="]["names"], pick_members_dict["&pickMembers%5B0%5D="]["values"]))
+    pick_members_2_dict = dict(zip(pick_members_dict["&pickMembers%5B1%5D="]["names"], pick_members_dict["&pickMembers%5B1%5D="]["values"]))
+
+    result = [(x, y) for x in values[0] for y in values[1]]
+    if pick_members_dict == {}:
+        url = 'https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=' + pid + '&cubeTimeFrame.startMonth='+ start_month + '&cubeTimeFrame.startYear=' + start_year + '&cubeTimeFrame.endMonth=' + end_month +'&cubeTimeFrame.endYear=' + end_year + '&referencePeriods=' + referencePeriods
         df = scrapper.simple_scrapper(url, filter_names).iloc[-1]
         latest_month = df["month"]
         
@@ -54,5 +53,31 @@ def data_pipeline_job(pid, table_name, pick_members_1, pick_members_2,filter_nam
             else:
                 df.to_sql(table_name, engine, if_exists="append", index=False)
                 print("Inserted data from " + x + " and " + y + " into " + table_name + " successfully")
-    
+    else:
+        for x, y in result:
+            geography=pick_members_1_dict[x]
+            type_of_employee=pick_members_2_dict[y]
+            url = 'https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=' + pid + '&pickMembers%5B0%5D='+ x + '&pickMembers%5B1%5D='+ y + '&cubeTimeFrame.startMonth='+ start_month + '&cubeTimeFrame.startYear=' + start_year + '&cubeTimeFrame.endMonth=' + end_month +'&cubeTimeFrame.endYear=' + end_year + '&referencePeriods=' + referencePeriods
+            df = scrapper.simple_scrapper(url, filter_names).iloc[-1]
+            latest_month = df["month"]
+            
+            query = "SELECT * FROM " + table_name +  " WHERE geography = '" + geography + "' AND type_of_employee = '" + type_of_employee + "' AND month = '" + latest_month + "';"
+
+            engine = create_engine(conn_string, connect_args={
+            "ssl": {
+                "ssl_ca": "ca.pem",
+                "ssl_cert": "client-cert.pem",
+                "ssl_key": "client-key.pem"
+            }
+        })
+            with engine.begin() as engine:
+                sql_data = pd.read_sql_query(query, engine)
+                sql_latest_month = sql_data["month"].values[0]
+                
+                if (sql_latest_month == latest_month):
+                    print(latest_month + " " + geography + " "+ type_of_employee + " data already exists in " + table_name + " table")
+                else:
+                    df.to_sql(table_name, engine, if_exists="append", index=False)
+                    print("Inserted data from " + x + " and " + y + " into " + table_name + " successfully")
+        
     print(bold_green +"Data pipeline job completed for " + table_name + " table at " + str(today) + reset)
--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def monthly_employment_by_industry_job():
     table_name = "monthly_employment_by_industry"
     pick_members_1 = {"names": ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10", "1.11", "1.12", "1.14", "1.15"], "values": ["Canada", "Newfoundland and Labrador", "Prince Edward Island", "Nova Scotia", "New Brunswick", "Quebec", "Ontario", "Manitoba", "Saskatchewan", "Alberta", "British Columbia", "Yukon", "Northwest Territories", "Nunavut"]}
     pick_members_2 = {"names": ["2.2", "2.3"], "values": ["Salaried employees paid a fixed salary", "Employees paid by the hour"]}
+    
+    pick_members_dict = {"&pickMembers%5B0%5D=": pick_members_1, "&pickMembers%5B1%5D=": pick_members_2}
     filter_names = ["Geography", "Type of employee"]
-    data_pipeline.data_pipeline_job(pid=pid, table_name=table_name, pick_members_1=pick_members_1, pick_members_2=pick_members_2, filter_names=filter_names)
+    data_pipeline.data_pipeline_job(pid=pid, table_name=table_name, pick_members_dict=pick_members_dict, filter_names=filter_names)
 
 test = "Hi there, welcome to my data pipeline!"
 scheduler = BackgroundScheduler()
