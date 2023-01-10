a02c3a9ba63057185ccdd3dfdaffb0f8c6d9397b
Boris Tsao <boris.tsao@gmail.com>
2023-01-09 18:09:59 -0800

    linking in data pipeline into flask

---
+++ data_pipeline.py
@@ -0,0 +1,51 @@
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import pandas as pd
+try:
+    from . import scrapper
+except:
+    import scrapper
+
+def data_pipeline_job(pid, table_name, pick_members_1, pick_members_2,filter_names) :
+    today = date.today()
+    four_months_ago = date.today() - relativedelta(months=+4)
+    start_month=str(four_months_ago.month).zfill(2)
+    start_year=str(four_months_ago.year)
+    end_month=str(today.month).zfill(2)
+    end_year=str(today.year)
+
+    referencePeriods = start_year + start_month + "01" + "%2C" + end_year + end_month + "28" 
+    pick_members_1_dict = dict(zip(pick_members_1["names"], pick_members_1["values"]))
+    pick_members_2_dict = dict(zip(pick_members_2["names"], pick_members_2["values"]))
+
+    result = [(x, y) for x in pick_members_1["names"] for y in pick_members_2["names"]]
+    ssl_args = {'ssl_ca': "/etc/ssl/cert.pem"}
+    
+    conn_string = 'mysql+pymysql://' + os.getenv("USERNAME") + ':' + os.getenv("PASSWORD") + '@' + os.getenv("HOST") + '/' + os.getenv("DATABASE") 
+
+    for x, y in result:
+        geography=pick_members_1_dict[x]
+        type_of_employee=pick_members_2_dict[y]
+        month= today.strftime("%B") + " " + str(today.year)
+        url = 'https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=' + pid + '&pickMembers%5B0%5D='+ x + '&pickMembers%5B1%5D='+ y + '&cubeTimeFrame.startMonth='+ start_month + '&cubeTimeFrame.startYear=' + start_year + '&cubeTimeFrame.endMonth=' + end_month +'&cubeTimeFrame.endYear=' + end_year + '&referencePeriods=' + referencePeriods
+        df = scrapper.simple_scrapper(url, filter_names).iloc[-1]
+        latest_month = df["month"]
+        
+        query = "SELECT * FROM " + table_name +  " WHERE geography = '" + geography + "' AND type_of_employee = '" + type_of_employee + "' AND month = '" + latest_month + "';"
+
+        engine = create_engine(conn_string, connect_args=ssl_args)
+        with engine.begin() as engine:
+            sql_data = pd.read_sql_query(query, engine)
+            sql_latest_month = sql_data["month"].values[0]
+            
+            if (sql_latest_month == latest_month):
+                print(latest_month + " " + geography + " "+ type_of_employee + " data already exists in " + table_name + " table")
+            else:
+                df.to_sql(table_name, engine, if_exists="append", index=False)
+                print("Inserted data from " + x + " and " + y + " into " + table_name + " successfully")
+    
+    print("Data pipeline job completed for " + table_name + " table at " + str(today))
--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from flask import Flask
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 import os
+try:
+    from . import data_pipeline
+except:
+    import data_pipeline
+
 app = Flask(__name__)
 
-test = "Hi there"
+def monthly_employment_by_industry_job():
+    pid="1410020101"
+    table_name = "monthly_employment_by_industry"
+    pick_members_1 = {"names": ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10", "1.11", "1.12", "1.14", "1.15"], "values": ["Canada", "Newfoundland and Labrador", "Prince Edward Island", "Nova Scotia", "New Brunswick", "Quebec", "Ontario", "Manitoba", "Saskatchewan", "Alberta", "British Columbia", "Yukon", "Northwest Territories", "Nunavut"]}
+    pick_members_2 = {"names": ["2.2", "2.3"], "values": ["Salaried employees paid a fixed salary", "Employees paid by the hour"]}
+    filter_names = ["Geography", "Type of employee"]
+    data_pipeline.data_pipeline_job(pid=pid, table_name=table_name, pick_members_1=pick_members_1, pick_members_2=pick_members_2, filter_names=filter_names)
 
+
+test = "Hi there, welcome to my data pipeline!"
+scheduler = BackgroundScheduler()
+# schedule an apscheduler job to run monthly_employment_by_industry_job every last day of the month
+job = scheduler.add_job(monthly_employment_by_industry_job, 'cron', day='day', hour=16, minute=00)
+
+scheduler.start()
+
+# job = scheduler.add_job(test_job, 'cron', day_of_week ='mon-sun', hour=16, minute=00)
+for job in scheduler.get_jobs():
+    job.modify(next_run_time=datetime.now())
 @app.route("/")
 def home():
     return "Hi there!"
